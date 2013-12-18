cde7db6eb26ace01d02049aa2b6d5f9d9930fc47
root <root@liuwei.(none)>
2013-12-18 14:04:25 +0800

    test SO_RCVBUF

---
+++ socket_option/so_rcvbuf.cpp
@@ -0,0 +1,30 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int main()
+{
+    int ret = 0;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == sock)
+    {
+        perror("socket");
+        return errno;
+    }
+
+    int opt = 1;
+    socklen_t len = sizeof(opt);
+    // net.ipv4.tcp_rmem is for global setting
+    ret = getsockopt(sock, SOL_SOCKET, SO_RCVBUF, &opt, &len);
+    if (-1 == ret)
+    {
+        perror("setsockopt");
+        return errno;
+    }
+
+    printf("SO_RCVBUF: %d\n", opt);
+
+    return 0;
+}
+

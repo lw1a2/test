64d19005827e5b26048a44996e061243e4c0802f
root <root@liuwei.(none)>
2014-02-25 10:42:23 +0800

    add

---
+++ shell_getopts/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage() {
+    prog=`basename $0`
+    echo "Usage: $prog <options>"
+    echo "Options:"
+    echo "    -a 			echo aaa"
+	echo "    -b 			echo bbb"
+	echo "    -c <arg>		echo arg"
+    echo "    -h			display this help and exit"
+    exit
+}
+
+while getopts "abc:h" Option
+do
+    case $Option in
+        a) echo "aaa";;		#无参数的
+        b) echo "bbb";;		#无参数的
+        c) echo "$OPTARG";;	#有参数的
+		h) usage;;			#帮助
+        *) usage;;			#如果参数输入有误，则打印完shell的错误后，会执行这里
+    esac
+done
+

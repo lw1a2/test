02097628ab8354b4fe635c950d43fe4ccb0525e4
lw1a2 <lw1a2.jing@gmail.com>
2015-03-16 11:20:28 +0800

    EvenTree: done

--- hackerrank/EvenTree/EvenTree.cpp
+++ hackerrank/EvenTree/EvenTree.cpp
@@ -3,8 +3,33 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <set>
 using namespace std;
 
+int cut_count = 0;
+set<int> s;
+
+void f(vector<vector<int> >& v, int i) {
+    s.insert(i);
+    // all adjacencies
+    for (int j = 0; j < v[i].size();) {
+        bool is_cut = false;
+        if (s.count(v[i][j]) == 0) {
+            f(v, v[i][j]);
+            if (!(v[v[i][j]].size() % 2)) {
+                vector<int>::iterator pos = find(v[i].begin(), v[i].end(), v[i][j]);
+                if (pos != v[i].end()) {
+                    v[i].erase(pos);
+                    is_cut = true;
+                }
+                ++cut_count;
+            }
+        }
+        if (!is_cut) {
+            ++j;
+        }
+    }
+}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
@@ -15,40 +40,12 @@ int main() {
         int ui, vi;
         cin >> ui >> vi;
         v[ui].push_back(vi);
+        v[vi].push_back(ui);
     }
 
-    // for (int i = 1; i <= N; ++i) {
-    //     if (!v[i].empty()) {
-    //         cout << i << ": ";
-    //         for (int j = 0; j < v[i].size(); ++j) {
-    //             cout << v[i][j] << ' ';
-    //         }
-    //         cout << endl;
-    //     }
-    // }
+    f(v, 1);
 
-    vector<bool> done(N + 1);
-    int count = 0;
-    while (count != N) {
-        for (int i = 1; i <= N; ++i) {
-            if (done[i]) {
-                continue;
-            }
-            if (v[i].size() == 1) {
-                done[i] = true;
-            } else {
-                bool all_adj_done = true;
-                for (int j = 0; j < v[i].size(); ++j) {
-                    if (!done[v[i]]) {
-                        all_adj_done = false;
-                    }
-                }
-                if (all_adj_done) {
-                    done[i] = true;
-                }
-            }
-        }
-    }
+    cout << cut_count << endl;
 
     return 0;
 }

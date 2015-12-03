8951c6bd4f14d6a2486e133f52ec679798a1ab7c
lw1a2 <lw1a2.jing@gmail.com>
2015-12-03 15:55:56 +0800

    update

--- sscanf/test_sscanf.c
+++ sscanf/test_sscanf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+void f()
 {
     char *line = "port1=12345";
     char port_name[16];
@@ -13,6 +13,27 @@ int main()
 
     printf("port_name: %s\n", port_name);
     printf("pci_id: %s\n", pci_id);
+}
+
+void f2()
+{
+    char *s = "/captcha.html?captcha=mapkh";
+    char captcha_name[2];
+    bzero(captcha_name, sizeof(captcha_name));
+    char captcha_value[6];
+    bzero(captcha_value, sizeof(captcha_value));
+    char prefix[256];
+    bzero(prefix, sizeof(prefix));
+    sscanf(s, "%[^?]?%[^=]=%s", prefix, captcha_name, captcha_value);
+
+    printf("prefix: %s\n", prefix);
+    printf("captcha_name: %s\n", captcha_name);
+    printf("captcha_value: %s\n", captcha_value);
+}
+
+int main()
+{
+    f2();
 
     return 0;
 }

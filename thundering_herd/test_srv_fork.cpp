3381f463d4cf6a874d926d640339347db857c51b
lw1a2 <lw1a2.jing@gmail.com>
2016-04-25 11:59:26 +0800

    test thundering herd

---
+++ thundering_herd/test_srv_fork.cpp
@@ -0,0 +1,70 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    struct sockaddr_in serv;
+    bzero(&serv, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(6666);
+    if (inet_pton(AF_INET, "172.22.15.230", &serv.sin_addr) != 1) {
+        printf("inet_pton server 172.22.15.230 failed\n");
+        return 1;
+    }
+
+    int ret = 0;
+    int sock = -1;
+    int on = 1;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == sock) {
+        perror("socket");
+        return errno;
+    }
+    ret = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    if (-1 == ret) {
+        perror("setsockopt");
+        return errno;
+    }
+    ret = bind(sock, (struct sockaddr*)&serv, sizeof(serv));
+    if (-1 == ret)
+    {
+        perror("bind server");
+        return errno;
+    }
+    ret = listen(sock, 5);
+    if (-1 == ret) {
+        perror("listen");
+        return errno;
+    }
+
+    // fork 3 children
+    for (int i = 0; i < 3; ++i) {
+        ret = fork();
+        if (ret == 0) {
+            // child
+            while (1) {
+                int new_sock = accept(sock, NULL, NULL);
+                if (new_sock < 0) {
+                    perror("accept");
+                }
+            }
+            return 0;
+        } else if (ret > 0) {
+            // father
+        } else {
+            perror("fork");
+            return errno;
+        }
+    }
+
+    sleep(9999);
+    return 0;
+}
+
